3d8defc4f33013893b0d654fcc75a5ef83fb9d72
ankitchamke <ankitchamke@gmail.com>
2025-06-06 16:04:14 +0530

    Learned JavaScript basic
    variables, math operations, functions, and simple DOM events using onclick

---
+++ Day 1/Challenges.js
@@ -0,0 +1,107 @@
+// // Write your first JavaScript Variable
+
+// // CHALLENGE 1
+// // 1. Create a variable, myAge, and set its value to your age
+// let myAge = 19
+// // 2. Log the myAge variable to the console
+// console.log(myAge)
+
+// -------------------------------------------------------------------------------------------------------------------------
+
+// // Basic Mathenatical operations
+
+// // CHALLENGE 2
+// // assume 1 human year = 7 dog years
+// // 1. Create two variables, myAge and humanDogRatio
+// let myAge = 19
+// let humanDogRatio = 7
+// // 2. Multiply the two together and store the result in myDogAge 
+// myDogAge = myAge * humanDogRatio
+// // 3. Log myDogAge to the console
+// console.log(myDogAge)
+
+// -------------------------------------------------------------------------------------------------------------------------
+
+// // Reassigning & Incrementing
+
+// // CHALLENGE 3
+// // Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// let bonusPoints = 50
+// bonusPoints = bonusPoints + 50
+// console.log(bonusPoints)
+// // Decrease it down to 25, and then finally increase it to 70
+// bonusPoints = bonusPoints - 75
+// console.log(bonusPoints)
+// bonusPoints = bonusPoints + 45
+// console.log(bonusPoints)
+// // Console.log the value after each step
+
+// -------------------------------------------------------------------------------------------------------------------------
+
+// // Onclick Event listener
+// function increment(){
+    //     console.log("Button is clicked")
+    // }
+    
+// -------------------------------------------------------------------------------------------------------------------------
+
+// // Using function to write less code
+// function countdown() {
+    //     console.log(5)
+    //     console.log(4)
+    //     console.log(3)
+    //     console.log(2)
+    //     console.log(1)
+    // }
+    
+    // // Setting up the the race 🏎 🏎 🏎
+    
+    // countdown()
+    
+    // // GO! 🏁
+    // // Players are running the race 🏎 💨
+    // // Race is finished! 🍾
+    // // Get ready for a new race 🏎 🏎 🏎
+    
+    // countdown()
+
+// -------------------------------------------------------------------------------------------------------------------------
+
+// // Write your first function
+
+// // CHALLENGE 5
+// // Create a function (you decide the name) that logs out the number 42 to the console
+// // Call/invoke the function
+
+// function function_name(){
+    //     console.log(42)
+    // }
+    
+    // function_name()
+
+// -------------------------------------------------------------------------------------------------------------------------
+
+// // Write a function that logs the sum
+
+// // CHALLENGE 6
+// let lap1 = 34
+// let lap2 = 33
+// let lap3 = 36
+
+// // Create a function that logs out the sum of all the lap times
+
+// function sum(){
+//         // let sum = lap1 + lap2 + lap3
+//         // console.log(sum)
+//         console.log(lap1 + lap2 + lap3)
+//     }
+//     sum()
+
+// -------------------------------------------------------------------------------------------------------------------------
+
+// Write a function that increments
+// CHALLENGE 7
+
+
+
+// -------------------------------------------------------------------------------------------------------------------------
