cb8868fc9881989e0d019863750d029fbd4104c3
ankitchamke <ankitchamke@gmail.com>
2025-06-08 23:25:46 +0530

    Day 2 of Js
    Practiced DOM manipulation and string handling in JavaScript — updated HTML with innerText, learned string concatenation, variable incrementing, and created interactive click functions.

---
+++ Day 2/Challenges.js
@@ -0,0 +1,100 @@
+
+// -------------------------------------------------------------------------------------------------------------------------
+
+// Write a function that increments
+// CHALLENGE 7
+
+// let lapsCompleted = 0
+// // Create a function that increments the lapsCompleted variable with one
+// function increments(){
+//     lapsCompleted = lapsCompleted + 1
+// }
+// // Run it three times
+// increments()
+// increments()
+// increments()
+
+
+// console.log(lapsCompleted)
+
+// -------------------------------------------------------------------------------------------------------------------------
+
+// // Increment on clicks
+// // CHALLENGE 8,9
+
+// // intialize the count as 0
+// // listen for clicks on the increment button
+// // increment the count variable when the button is clicked (log it out)
+// // change the count-el in the HTML to reflect the new count // camelCase
+// // console.log(countEl)
+
+// let countEl = document.getElementById("count-el")
+// count = 0
+// function increment(){
+//     count = count+1
+//     countEl.innerText = count
+//     console.log(count)
+// }
+
+// -------------------------------------------------------------------------------------------------------------------------
+
+// The Document Object Model
+// Create the save button
+// CHALLENGE 10
+// 1. Create a function, save(), which logs out the count when it's called
+// function save(){
+//     console.log(count)
+// }
+
+// -------------------------------------------------------------------------------------------------------------------------
+
+// // What is a string?
+// let username = "per"
+// // CHALLENGE 11
+// // Create a variable, message, that stores the string: "You have tree new notifications"
+// let message = "You have tree new notifications"
+// // console.log(message +", "+ username + "!")
+
+// // CHALLENGE 12
+// // Create a variable, messageToUser, that contains the message we have logged
+// let messageToUser = message +", "+ username + "!"
+// // replace l56-
+// console.log(messageToUser)
+
+// -------------------------------------------------------------------------------------------------------------------------
+
+// Log a greeting to the console 
+// CHALLENGE 13
+
+// Create two variables, name and greeting. The name variable should store your name,
+// and the greeting should store e.g. "Hi, my name is "
+// Create a third variable, myGreeting, that contatenates the two strings
+// Log myGreeting to the console
+
+// let name = "Ankit"
+// let greeting = "Hi, my name is " 
+// let myGreeting =  greeting + name
+// console.log(myGreeting)
+
+// -------------------------------------------------------------------------------------------------------------------------
+
+// // Strings vs. Numbers
+// CHALLENGE 14
+// console.log(4 + 5) // 9
+// console.log("2" + "4") // "24"
+// console.log("5" + 1) // "51"
+// console.log(100 + "100") // "100100"
+
+// -------------------------------------------------------------------------------------------------------------------------
+
+// // CHALLENGE 15
+// // Grab the welcome-el paragraph and store it in a variable called welcomeEl
+// let welcomeEl = document.getElementById("welcome-el")
+// // Create two variables (name & greeting) that contains your name
+// let name = "Ankit"
+// // and the greeting we want to render on the page
+// let greeting = "Welcome "
+// // Render the welcome message using welcomeEl.innerText
+// welcomeEl.innerText = greeting + name
+
+// -------------------------------------------------------------------------------------------------------------------------
