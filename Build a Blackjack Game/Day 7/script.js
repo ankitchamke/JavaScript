3910aa9fea71cf1185ae8c68b8eeebcfd3f648dd
ankitchamke <ankitchamke@gmail.com>
2025-06-19 23:56:15 +0530

    Day 14
    Added hasBlackJack and isAlive state tracking in the Blackjack game 🃏
    
    Practiced boolean expressions and logical conditions
    
    Used if-else control flow to determine game outcomes
    
    Stored messages in a variable before logging
    
    Started exploring basic CSS styling and linking external stylesheets

---
+++ Build a Blackjack Game/Day 7/script.js
@@ -0,0 +1,86 @@
+// // Add the hasBlackJack variable
+
+// let firstCard = 10
+// let secondCard = 11
+// let sum = firstCard + secondCard
+// let hasBlackJack = false
+
+// if (sum <= 20) {
+//     console.log("Do you want to draw a new card? 🙂")
+// } else if (sum === 21) {
+//     console.log("Wohoo! You've got Blackjack! 🥳")
+//     hasBlackJack = true
+// } else {
+//     console.log("You're out of the game! 😭")
+// }
+
+// // CASH OUT!
+// console.log(hasBlackJack)
+
+// ------------------------------------------------------------------------
+
+// CHALLENGE - 4
+// let firstCard = 10
+// let secondCard = 11
+// let sum = firstCard + secondCard + 9
+// let hasBlackJack = false
+// // 1. Create a variable called isAlive and assign it to true
+// let isAlive = true
+// // 2. Flip its value to false in the appropriate code block 
+// if (sum <= 20) {
+//     console.log("Do you want to draw a new card? 🙂")
+// } else if (sum === 21) {
+//     console.log("Wohoo! You've got Blackjack! 🥳")
+//     hasBlackJack = true
+// } else {
+//     console.log("You're out of the game! 😭")
+//     isAlive = false
+// }
+
+// // 3. Log it out to check that you're doing it right
+
+// console.log(isAlive)
+
+// Let's practice boolean conditions-------------------------------------------
+
+// CHALLENGE - 4
+// console.log(4 === 3)  // False
+// console.log(5 > 2)    // True
+// console.log(12 > 12)  // False
+// console.log(3 < 0)    // False
+// console.log(3 >= 3)   // True
+// console.log(11 <= 11) // True
+// console.log(3 <= 2)   // False
+
+// Add the message variable----------------------------------------------------
+
+// CHALLENGE - 5
+// let firstCard = 10
+// let secondCard = 11
+// let sum = firstCard + secondCard
+// let hasBlackJack = false
+// let isAlive = true
+// // 1. Declare a variable called message and assign its value to an empty string
+// let message = ""
+
+// // 2. Reassign the message variable to the string we're logging out
+// if (sum <= 20) {
+//     message = "Do you want to draw a new card? 🙂"
+// } else if (sum === 21) {
+//     message = "Wohoo! You've got Blackjack! 🥳"
+//     hasBlackJack = true
+// } else {
+//     message = "You're out of the game! 😭"
+//     isAlive = false
+// }
+
+// // 3. Log it out!
+// console.log(message)
+
+// Link to stylesheet----------------------------------------------------------
+// CHALLENGE - 6
+// Add basic styling-----------------------------------------------------------
+// CHALLENGE - 7 in style.css
+
+
+
