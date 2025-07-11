3b37ff84d1f8d4fbb55701c78319b71d61293b22
ankitchamke <ankitchamke@gmail.com>
2025-07-11 23:27:03 +0530

    .

---
+++ Build a Chrome Extension/Day 5/aside.js
@@ -0,0 +1,25 @@
+const credits = 0
+
+if (credits) {
+    console.log("Let's play 🎰")
+} else {
+    console.log("Sorry, you have no credits 😭")
+}
+
+// truthy
+// falsy
+
+// false
+// 0
+// ""
+// null -> how you as a developer signalize emptiness
+// undefined -> how JavaScript signalizes emptiness
+// NaN
+
+
+// console.log(  Boolean("")   ) // False
+// console.log(  Boolean("0")  ) // True
+// console.log(  Boolean(100)  ) // True
+// console.log(  Boolean(null) ) // False
+// console.log(  Boolean([0])  ) // True
+// console.log(  Boolean(-0)   ) // False
