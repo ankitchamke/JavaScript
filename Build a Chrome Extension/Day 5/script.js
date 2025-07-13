0cf5f772c843eab735c59c0e9a6168ad4e03cb65
ankitchamke <ankitchamke@gmail.com>
2025-07-13 18:11:24 +0530

    Day 36

--- Build a Chrome Extension/Day 5/script.js
+++ Build a Chrome Extension/Day 5/script.js
@@ -3,12 +3,30 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
-let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+// i. Store the delete button in a deleteBtn variable
+const deleteBtn = document.getElementById("delete-btn")
+
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+console.log(leadsFromLocalStorage)
 
 // 1. Check if leadsFromLocalStorage is truthy
 // 2. If so, set myLeads to its value and call renderLeads()
 
-if
+if(leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
+
+// ii. Listen for double clicks on the delete button (google it!)
+// iii. When clicked, clear localStorage, myLeads, and the DOM
+
+deleteBtn.addEventListener("dblclick", function() {
+    // console.log("dpl")
+    localStorage.clear()
+    myLeads = []
+    renderLeads()
+})
+
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
